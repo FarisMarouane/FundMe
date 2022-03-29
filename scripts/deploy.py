dded26ebf761344f13839fd2d45cbd35d5a20e4a
Marouane Faris <faris.marouane@gmail.com>
2022-03-29 19:03:48 +0200

    Bring back local development option

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,16 +1,15 @@
 import os
+from brownie import FundMe, MockV3Aggregator, config, network, chain
+from scripts.helpers import create_account, LOCAL_BLOCKCHAIN_ENVIRONMENTS, deploy_mocks
 
-from brownie import FundMe, config, network, chain
-from scripts.helpers import create_account
 
+def deploy(compaignDuration, fundRaiserAddress):
 
-def deploy():
-    create_account()
-    compaignDuration = os.environ.get("duration") or 5
-    fundRaiserAddress = os.environ.get("fundRaiserAddress")
-    price_feed = config["networks"][network.show_active()]["eth_usd_price_feed"]
-
-    print("fundRaiserAddress***", fundRaiserAddress)
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        price_feed = config["networks"][network.show_active()]["eth_usd_price_feed"]
+    else:
+        deploy_mocks()
+        price_feed = MockV3Aggregator[-1].address
 
     fund_me = FundMe.deploy(
         price_feed,
@@ -20,22 +19,29 @@ def deploy():
     )
 
     print("fund_me contract", fund_me)
+    return
 
-    return fund_me
 
-
-def withdraw(contract):
+def withdraw(fundRaiserAddress):
     fund_me = FundMe[-1]
     while True:
         if (
             chain.time() - fund_me.fundRaiseCreationTime() > fund_me.lockPeriod() * 60
         ):  # Nb of minutes * 60 sec
-            print("chain.time", chain.time())
-            print("fund_me.fundRaiseCreationTime", fund_me.fundRaiseCreationTime())
-            fund_me.withdraw({"from": contract})
+            print("chain.time***", chain.time())
+            print("fund_me.fundRaiseCreationTime***", fund_me.fundRaiseCreationTime())
+            print("lockPeriod***", fund_me.lockPeriod())
+            fund_me.withdraw({"from": fundRaiserAddress})
             break
 
 
 def main():
-    contract = deploy()
-    # withdraw(contract)
+    account = create_account()
+    compaignDuration = os.environ.get("duration") or 2
+    if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        fundRaiserAddress = account
+    else:
+        fundRaiserAddress = os.environ.get("fundRaiserAddress")
+
+    deploy(compaignDuration, fundRaiserAddress)
+    withdraw(fundRaiserAddress)
--- scripts/helpers.py
+++ scripts/helpers.py
@@ -1,6 +1,21 @@
-from brownie import config, accounts
+from brownie import config, accounts, network, MockV3Aggregator
 from web3 import Web3
+
+LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["development", "ganache-local"]
+DECIMALS = 8
+STARTING_PRICE = 200000000000
 
 
 def create_account():
-    return accounts.add(config["wallets"]["primary_key"])
+    if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        return accounts[0]
+    else:
+        return accounts.add(config["wallets"]["primary_key"])
+
+
+def deploy_mocks():
+    print(f"The active network is {network.show_active()}")
+    print("Deploying Mocks...")
+    if len(MockV3Aggregator) <= 0:
+        MockV3Aggregator.deploy(DECIMALS, STARTING_PRICE, {"from": accounts[0]})
+    print("Mocks Deployed!")
